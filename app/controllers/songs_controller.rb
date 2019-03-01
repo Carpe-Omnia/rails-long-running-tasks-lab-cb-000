52eaa4a786edabd4a92713d784e4f0e33e5787c9
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-03-01 20:50:59 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,10 @@
 class SongsController < ApplicationController
   require 'csv'
 
+  def upload
+    CSV.foreach(params[:songs].path, headers: true) do |song|
+      Artist.find_or_create_by(name: song[1])
+      Song.create(title: song[0])
   def index
     @songs = Song.all
   end
